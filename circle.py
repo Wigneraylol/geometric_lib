7e1ad8916f2732440b6f2c57164a3bbfdb4188aa
Wigner <wigneraylol@gmail.com>
2025-10-13 19:21:49 +0300

    Added documentation

--- circle.py
+++ circle.py
@@ -2,9 +2,27 @@ import math
 
 
 def area(r):
+    ''' Возвращает площадь круга с заданным радиусом.
+        Параметры:
+            r (int or float): радиус круга
+        
+        Возвращаемое значение: 
+           (float): площадь круга
+           
+        Пример вызова: 
+            circle_area = area(5) # circle_area = 78.53981633974483'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    ''' Возвращает периметр круга с заданным радиусом.
+        Параметры:
+            r (int or float): радиус круга
+        
+        Возвращаемое значение: 
+            (float): периметр круга
+        
+        Пример вызова: 
+            circle_perimeter = perimeter(5) # circle_perimeter = 31.41592653589793'''
     return 2 * math.pi * r
 
